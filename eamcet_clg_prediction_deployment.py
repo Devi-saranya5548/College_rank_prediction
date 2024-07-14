152b31b8e59581ece487b71ea140aab83abb876c
Devi-saranya5548 <155404986+Devi-saranya5548@users.noreply.github.com>
2024-07-14 16:19:10 +0530

    Update eamcet_clg_prediction_deployment.py

--- eamcet_clg_prediction_deployment.py
+++ eamcet_clg_prediction_deployment.py
@@ -1,7 +1,7 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-import pickle
+# import streamlit as st
+# import pandas as pd
+# import numpy as np
+# import pickle
 # import openpyxl
 
 # Load the trained model
